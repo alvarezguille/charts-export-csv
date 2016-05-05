f61f5672b5752aafe16021ee518b87d59aaa8c89
Guillermo Alvarez <guillermo@vaadin.com>
2016-05-05 21:53:08 +0300

    Add basic chart
    Added Vaadin Charts dependency
    Added a basic SPLINE chart to existing UI

--- src/main/java/uy/com/chartexport/MyUI.java
+++ src/main/java/uy/com/chartexport/MyUI.java
@@ -2,14 +2,19 @@ package uy.com.chartexport;
 
 import javax.servlet.annotation.WebServlet;
 
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.AxisTitle;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.DataLabels;
+import com.vaadin.addon.charts.model.ListSeries;
+import com.vaadin.addon.charts.model.PlotOptionsLine;
+import com.vaadin.addon.charts.model.YAxis;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -27,21 +32,51 @@ public class MyUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
+        layout.addComponent(getChart());
         
         setContent(layout);
+    }
+
+    private Chart getChart() {
+        Chart chart = new Chart();
+        chart.setHeight("450px");
+        chart.setWidth("100%");
+
+        Configuration configuration = chart.getConfiguration();
+        configuration.getChart().setType(ChartType.SPLINE);
+
+        configuration.getTitle().setText("Monthly Average Temperature");
+        configuration.getSubTitle().setText("Source: WorldClimate.com");
+
+        configuration.getxAxis().setCategories("Jan", "Feb", "Mar", "Apr",
+                "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+        YAxis yAxis = configuration.getyAxis();
+        yAxis.setTitle(new AxisTitle("Temperature (°C)"));
+
+        configuration
+                .getTooltip()
+                .setFormatter(
+                        "'<b>'+ this.series.name +'</b><br/>'+this.x +': '+ this.y +'°C'");
+
+        PlotOptionsLine plotOptions = new PlotOptionsLine();
+        plotOptions.setDataLabels(new DataLabels(true));
+        configuration.setPlotOptions(plotOptions);
+
+        ListSeries ls = new ListSeries();
+        ls.setName("Tokyo");
+        ls.setData(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3,
+                13.9, 9.6);
+        configuration.addSeries(ls);
+
+        ls = new ListSeries();
+        ls.setName("London");
+        ls.setData(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6,
+                4.8);
+        configuration.addSeries(ls);
+
+        return chart;
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
